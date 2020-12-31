b9b10d5c08658fb7c844f40a3eeaf6fb24da62de
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2020-12-31 16:32:36 +0100

    Delete models.py

--- autogrow/software/raspberry pi/models.py
+++
@@ -1,8 +0,0 @@
-class Plante():
-    def __init__ (self,nom,temps,humidite,chaleur,arrosage,ph):
-        self.name = nom
-        self.time = temps #en secondes
-        self.humidite = humidite #en %
-        self.chaleur = chaleur #en degrés celsus
-        self.arrosage = arrosage #en jour
-        self.ph = ph 

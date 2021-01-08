5527426193dbc4807b3a57867c739628dc466b9e
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-01-08 18:51:46 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,9 +1,7 @@
 <?php
 echo "Setting up Files\n";
 exec("export DISPLAY=:0.0");
-exec("cd autogrow/software");
-exec("mv 'raspberry pi' raspi");
-exec("cd raspi ");
+exec("cd autogrow/software/raspi ");
 exec("python3 frontend.py");
 echo "let's go :)";
 ?>

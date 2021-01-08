300c148992da36e759081d0332baf7c09529c561
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-01-08 18:26:42 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -3,8 +3,5 @@ echo "Setting up Files\n";
 exec("export DISPLAY=:0.0");
 exec("cd autogrow/autogrow/software/'raspberry pi' ");
 exec("python3 frontend.py");
-exec("sudo chmod 777 /etc/apache2/apache2.conf");
-exec("sudo cp -n apachange /usr/bin");
-exec("sudo \cp  apachange.php /usr/bin");
 echo "You are Ready to Use the Command\n";
 ?>

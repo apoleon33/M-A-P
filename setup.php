ddb308c9152f0f4ee0c1c54b94cd7d97cdafffba
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-01-08 18:22:19 +0100

    Delete setup.php

--- setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Setting up Files\n";
-exec("export DISPLAY=:0.0");
-exec("sudo chmod +x apachange.php");
-exec("sudo cp -n apachange /usr/bin");
-exec("sudo cp -n apachange.php /usr/bin");
-echo "You are Ready to Use the Command\n";
-?>

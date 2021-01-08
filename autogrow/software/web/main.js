e6159ce7b898382d76c000cb1046da071652b32a
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-01-08 22:41:04 +0100

    Create main.js

---
+++ autogrow/software/web/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}

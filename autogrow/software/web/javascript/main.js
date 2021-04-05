d8d8a5479f02cce91a14e432f57bb99315c36b37
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-04-05 19:37:29 +0200

    Delete main.js

--- autogrow/software/web/javascript/main.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}

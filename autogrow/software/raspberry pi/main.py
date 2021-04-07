dfda2825f023f7410f97ec04cf5d5269897dedf6
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2021-04-07 14:12:48 +0200

    Update main.py

--- autogrow/software/raspberry pi/main.py
+++ autogrow/software/raspberry pi/main.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import dht11
 from datagest import *
 import serial
+import datetime
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
@@ -18,10 +19,14 @@ def temp_hum():
     	print("Temperature: %-3.1f C" % result.temperature)
     	print("Humidity: %-3.1f %%" % result.humidity)
     	add_temp(0, result.temperature)
-	add_hum(0, result.humidity)
+		add_hum(0, result.humidity)
 	else:
     	print("Error: %d" % result.error_code)
-def relai(lampe:bool,res:bool):
+def temp():
+	return result.temperature
+def hum():
+	return result.humidity
+def relai(lampe:bool=False,res:bool=False):
 	if lampe ==True:
 		GPIO.output(5,True)
 	elif res==True:
@@ -34,16 +39,31 @@ def pompe(pump:bool):
 		GPIO.output(13,True)
 	else:
 		GPIO.output(13,False)
-def arduino():
-	capt1=[]
-	capt2=[]
+def arduino(var=False):
 	data = arduino.readline()
-	for i in data:
-		if i=="a":
-			var=True
-		if var==True:
-			capt2.append(str(i))
-		else:
-			capt1.append(str(i))
-	capt1_decoded = str(capt1[0:len(capt1)].decode("utf-8"))
-	capt2_decoded = str(capt2.decode("utf-8"))
+  	decoded_bytes1 = str(data[0:3].decode("utf-8"))
+  	decoded_bytes2 = str(data[3:len(data)-2].decode("utf-8"))
+def lever(x:int):
+	if x==1 or x==12: #janvier/decembre
+		matin=9
+	if x==5 or x==8 or x==9:#mai/aout/septembre
+		matin=7
+	if x==6 or x==7:#juin/juillet
+		matin=6
+	else:#le reste
+		matin=8
+	return matin
+def coucher(x:int):
+	if x<6:
+		soir=16+x
+	if x==7:
+		soir=22
+	if x==8 or x==9:
+		soir=21
+	if x==10:
+		soir=20
+	if x==11:
+		soir=18
+	if x==12:
+		soir=17
+	return soir

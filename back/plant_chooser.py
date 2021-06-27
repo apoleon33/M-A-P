62f7e41775bee28f6967cbf6b85b39f1b87acce2
apoleon33 <simondumesny@gmail.com>
2021-06-27 18:55:09 +0200

    now work (almost fine)

--- back/plant_chooser.py
+++ back/plant_chooser.py
@@ -16,9 +16,9 @@ for i in file:
 chose=int(input("choisissez votre plante dans la liste ci dessus (par leur numéro)"))
 for y in file:
 	if u==chose:
-		yeah= open('home/pi/autogrow/data/choice.txt',"a")
+		yeah= open('/home/pi/autogrow/data/choice.txt',"w")
 		yeah.write(file[y]["signe"])
 		print("done!")
 		yeah.close()
 	u+=1
-port.write(file[y]["signe"])
+port.write(file[y]["signe"].encode())

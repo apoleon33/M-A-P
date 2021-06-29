39e1e6660ff6153a24771a9cb20945a231ff3e8e
579 <simondumesny@gmail.com>
2021-06-29 21:32:19 +0200

    updated things

---
+++ front/src/app.js
@@ -0,0 +1,4 @@
+function home(){
+	var house = document.getElementById('main_box');
+	house.innerHTML = '<h1>damn</h1>'
+}
--- launch.sh
+++ launch.sh
@@ -2,9 +2,11 @@
 root=`pwd`
 sudo pip install -r requirements.txt
 cd
-if [ -e "choice.txt" ];then
+if [ -e "/home/pi/autogrow/data/choice.txt" ];then
 	echo "plant already choosed"
 else
 	python3 /home/pi/autogrow/back/plant_chooser.py
 fi
-python3 /home/pi/autogrow/back/serial_test.py
+#python3 /home/pi/autogrow/back/serial_test.py
+cd /autogrow/front
+electron .

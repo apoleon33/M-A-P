ac14cce2e27d63dbe98b2345a453e7b59373535e
579 <simondumesny@gmail.com>
2021-07-14 21:57:44 +0200

    switched from google chart to chart.js

--- front/src/app.js
+++ front/src/app.js
@@ -20,72 +20,61 @@ function home(){
 	})
 }
 function humidity(){
+	var house = document.getElementById('main_box');
+	house.innerHTML = '<h3>humiditée:</h3>'
+	house.innerHTML += '<canvas id="myChart"></canvas>'
+	var ctx = document.getElementById('myChart');
 	ipcRenderer.send('need-hum', 'now')
 	ipcRenderer.on('humidity', (event, arg) => {
 		var vide =100-arg
-		console.log(arg)
-		console.log(vide)
-		google.charts.load("current", {packages:["corechart"]});
-	      google.charts.setOnLoadCallback(drawChart);
-	      function drawChart() {
-	        var data = google.visualization.arrayToDataTable([
-	          ['humiditée', 'pourcentage'],
-	          ['humiditée',     arg],
-	          ['', vide]
-	        ]);
-
-	        var options = {
-	          title: 'humiditée',
-	          pieSliceText: 'none',
-	          pieHole: 0.4,
-	          pieStartAngle: 215,
-	          pieSliceBorderColor:'grey',
-	          backgroundColor: {
-	          	stroke:'grey',
-	          	fill: 'grey'
-	          },
-	          slices: {
-            	0: { color: '#0080FF' },
-            	1: { color: 'transparent' }
-          }
-          }
-	        var chart = new google.visualization.PieChart(document.getElementById('main_box'));
-	        chart.draw(data, options);
-	      }
+		var xValues = ["humiditée", ""];
+		var yValues = [55, 49];
+		var barColors = ["blue", "transparent"];
+		new Chart(ctx, {
+	  type: "doughnut",
+	  data: {
+	    labels: xValues,
+	    datasets: [{
+	      backgroundColor: barColors,
+	      borderColor:'grey',
+	      data: yValues
+	    }]
+	  },
+	  options: {
+	    title: {
+	      display: false
+	    },
+	    rotation:215,
+	  }
+	});
 	})
 }
 function temperature(){
+	var house = document.getElementById('main_box');
+	house.innerHTML = '<h3>temperature:</h3>'
+	house.innerHTML += '<canvas id="myChart"></canvas>'
+	var ctx = document.getElementById('myChart');
 	ipcRenderer.send('need-temp', 'now')
 	ipcRenderer.on('temperature', (event, arg) => {
-		google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['heure', 'temperature','temperature conseillé'],
-          ['0',  45,30],
-          ['5',  37,30],
-          ['10',  34,30],
-          ['15', 32,30],
-          ['20', 26,30],
-        ]);
-
-        var options = {
-          title: 'température',
-          curveType: 'function',
-          backgroundColor: {
-	          	stroke:'grey',
-	          	fill: 'grey'
-	      },
-	      colors:['red','black'],
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('main_box'));
-
-        chart.draw(data, options);
-      }
-
+		var xValues = [0,10,20,30,40,50,60,70,80,90,100];
+		new Chart("myChart", {
+		  type: "line",
+		  data: {
+		    labels: xValues,
+		    datasets: [{
+		      data: [36,30,27,26,30,33,31,30,28,29,30],
+		      borderColor: "red",
+		      fill: false
+		    },{
+		      data: [30,30,30,30,30,30,30,30,30,30,30],
+		      borderColor: "black",
+		      fill: false
+		    }]
+		  },
+		  options: {
+		    legend: {display: false}
+		  }
+		});
 	})
 }
 home()

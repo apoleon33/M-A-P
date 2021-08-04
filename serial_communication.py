cdfa3eed23f0f7c66347bb3fd7c62476b4c0e940
579 <simondumesny@gmail.com>
2021-08-04 20:07:14 +0200

    finished backend (theorically)

--- serial_communication.py
+++ serial_communication.py
@@ -1,30 +1,49 @@
 import serial
 import os
-humi = "/front/data/hum.txt"
-temp_now = "/front/data/temp_0.txt"
-temp_10h = "/front/data/temp_10.txt"
-temp_20h = "/front/data/temp_20.txt"
-temp_30h = "/front/data/temp_30.txt"
+import time
+import json
+vide=""
+humi = "front/data/hum.txt"
+temp_now = "front/data/temp_0.txt"
+temp_10h = "front/data/temp_10.txt"
+temp_20h = "front/data/temp_20.txt"
+temp_30h = "front/data/temp_30.txt"
+with open("front/data/plant.json","r") as main:
+	fire=json.load(main)
+with open("front/data/choice.txt") as iowa:
+	oklahoma= iowa.readlines()
+	vide=vide.join(oklahoma)
 def replace(before:str,after:str):
 	os.remove(after)
+	string=""
 	with open(before,"r") as temp:
 		tempe = temp.readlines()
 	with open(after,"w") as tem:
-		tem.write(tempe)
+		tem.write(string.join(tempe))
 def writing(file:str,don):
 	os.remove(file)
 	with open(file,"w") as files:
 		files.write(don)
 while True:
-	with serial.Serial("/dev/ttyACM0", baudrate=9600,timeout=39600) as port:
-		data = port.readline()
-		temperature = str(data[0:4].decode("utf-8"))
-		humidity = str(data[4:len(data)-2].decode("utf-8"))
-		replace(temp_20h,temp_30h)
-		replace(temp_10h,temp_20h)
-		replace(temp_now,temp_10h)
-		writing(temp_now,temperature)
-		writing(humi,humidity)
-		os.remove(temp_now)
-		with open(temp_now,"w") as temp_n:
-			temp_n.write(temperature)
+	try:
+		with serial.Serial("/dev/ttyACM0", baudrate=9600,timeout=39600) as port:
+			data1 = port.readline()
+			data2 = port.readline()
+			print(data1)
+			print(data2)
+			temperature = str(data1[0:len(data1)-2].decode("utf-8"))
+			humidity = str(data2[0:len(data2)-2].decode("utf-8"))
+			print(temperature)
+			print(humidity) #
+			replace(temp_20h,temp_30h)
+			replace(temp_10h,temp_20h)
+			replace(temp_now,temp_10h)
+			writing(temp_now,temperature)
+			writing(humi,humidity)
+			os.remove(temp_now)
+			with open(temp_now,"w") as temp_n:
+				temp_n.write(temperature)
+				pass
+	except:
+		break
+print("plus de connection série, fin du script")

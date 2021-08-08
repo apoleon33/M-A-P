1f4060dc6f5e58ad68c6563412d7171c9b47f93b
579 <simondumesny@gmail.com>
2021-08-08 18:17:41 +0200

    updated launch.sh

--- launch.sh
+++ launch.sh
@@ -4,9 +4,37 @@ sudo pip install -r requirements.txt
 cd front
 npm install
 cd
+function arduino {
+	echo "reseting arduino..."
+	cd $root/reset
+	arduino-cli compile --fqbn arduino:avr:uno reset.ino 
+	arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno reset.ino
+	sleep 1
+	cd $root/DigitalReadSerial
+	arduino-cli compile --fqbn arduino:avr:uno DigitalReadSerial.ino 
+	arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno DigitalReadSerial.ino
+}
+function checkout {
+	echo "plante déjas choisi, voulez-vous en choisir une nouvelle? [o/n]"
+	read rlly
+	if [ "$rlly" = "o" ]
+	then
+		arduino
+		rm "$root/front/data/choice.txt"
+		cd $root
+		python3 plant_chooser.py
+	elif [ "$rlly" = "n" ]
+	then
+		echo "ok"
+	else
+		echo "réponse invalide"
+		checkout
+	fi
+}
 if [ -e "$root/front/data/choice.txt" ]
 then
-	echo "plant already choosed"
+	clear
+	checkout
 else
 	cd $root
 	python3 plant_chooser.py

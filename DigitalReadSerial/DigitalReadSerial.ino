c301318013b175473e3dfa8ff05f854c8afc3a22
579 <simondumesny@gmail.com>
2021-08-21 18:40:12 +0200

    updated smting now the problem shoudl not come from the software lol

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -4,12 +4,12 @@
 #define dht_apin 4
 dht11 DHT11;
 RTC_DS3231 rtc;
-const int capt_1 = A0;
-const int capt_2 = A1;
-const int pompe = 6;
-const int resistance = 3;
-String trad1, trad2;
-int taux1,taux2,eau_hiver,eau_ete,temperature_hiver,temperature_ete,inputString,junk,a,Montenegro;
+int capt_1 = A0;
+int capt_2 = A1;
+int pompe = 12;
+int resistance = 3;
+String trad1, trad2,eau_hiver,eau_ete;
+int taux1,taux2,temperature_hiver,temperature_ete,inputString,junk,a,Montenegro;
 void setup(){
     Serial.begin(9600);
     pinMode(capt_1,INPUT);
@@ -27,20 +27,20 @@ void setup(){
     Montenegro=EEPROM.read(44);
     switch (Montenegro){
       case 1:
-        eau_hiver='pot';
-        eau_ete='coupelle';
+        eau_hiver="pot";
+        eau_ete="coupelle";
         temperature_hiver= 12;
         temperature_ete= 25;
         break;
       case 2:
-       eau_hiver='pot';
-       eau_ete='pot';
+       eau_hiver="pot";
+       eau_ete="pot";
        temperature_hiver= 10;
        temperature_ete= 25;
        break;
       case 3:
-       eau_hiver='pot';
-       eau_ete='pot';
+       eau_hiver="pot";
+       eau_ete="pot";
        temperature_hiver= 10;
        temperature_ete= 25;
        break;
@@ -58,24 +58,24 @@ void setup(){
               }
               switch (inputString){
                   case 'A':
-                      eau_hiver='pot';
-                      eau_ete='coupelle';
+                      eau_hiver="pot";
+                      eau_ete="coupelle";
                       temperature_hiver= 12;
                       temperature_ete= 25;
                       a=1;
                       EEPROM.write(44,1);
                       break;
                   case 'B':
-                      eau_hiver='pot';
-                      eau_ete='pot';
+                      eau_hiver="pot";
+                      eau_ete="pot";
                       temperature_hiver= 10;
                       temperature_ete= 25;
                       a=1;
                       EEPROM.write(44,2);
                       break;
                   case 'C':
-                      eau_hiver='pot';
-                      eau_ete='pot';
+                      eau_hiver="pot";
+                      eau_ete="pot";
                       temperature_hiver= 10;
                       temperature_ete= 25;
                       a=1;
@@ -91,38 +91,44 @@ void setup(){
 }
 void loop(){
     int chk = DHT11.read(dht_apin);
-    DateTime time = rtc.now();
+    DateTime now = rtc.now();
     taux1=map(analogRead(capt_1),0,1024,0,100);
     taux2=map(analogRead(capt_2),0,1024,0,100);
-    if ( time.month() >= 4 and time.month() <= 9){//ete
+    if ( now.month() >= 4 and now.month() <= 9){//ete
         if (DHT11.temperature < temperature_ete){//gestion temperature
-            digitalWrite(resistance,HIGH);
-            while(DHT11.temperature < temperature_ete);
+            while(DHT11.temperature < temperature_ete){
+              digitalWrite(resistance,HIGH);
+            }
         }
         digitalWrite(resistance,LOW);
-        if (taux1 < 20 and eau_ete=='coupelle'){
-            digitalWrite(pompe,HIGH);
-            while (taux1 < 20 and eau_ete=='coupelle');
+        if (taux1 < 20 and eau_ete=="coupelle"){
+          while (taux1 < 20 and eau_ete=="coupelle"){
+            digitalWrite(12,HIGH);
+          }
         }
-        if (taux2 < 20 and eau_ete=='pot'){
+        if (taux2 < 20 and eau_ete=="pot"){
+          while (taux2 < 20 and eau_ete=="pot"){
             digitalWrite(pompe, HIGH);
-            while (taux2 < 20 and eau_ete=='pot');
+          }
         }
         digitalWrite(pompe,LOW);
     } 
     else{//hiver
         if (DHT11.temperature < temperature_hiver){//gestion temperature
+          while(DHT11.temperature < temperature_hiver){
             digitalWrite(resistance,HIGH);
-            while(DHT11.temperature < temperature_hiver);
+          }
         }
         digitalWrite(resistance,LOW);
-        if (taux1 < 20 and eau_hiver=='coupelle'){
-            digitalWrite(pompe,HIGH);
-            while (taux1 < 20 and eau_hiver=='coupelle');
+        if (taux1 < 20 and eau_hiver == "coupelle"){
+          while (taux1 < 20 and eau_hiver=='coupelle'){
+            digitalWrite(12,HIGH);
+          }
         }
-        if (taux2 < 20 and eau_hiver=='pot'){
+        if (taux2 < 20 and eau_hiver=="pot"){
+          while (taux2 < 20 and eau_hiver=='pot'){
             digitalWrite(pompe, HIGH);
-            while (taux2 < 20 and eau_hiver=='pot');
+         }
         }
         digitalWrite(pompe,LOW);
     }

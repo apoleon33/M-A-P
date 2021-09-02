b4404707be7e963f31e3f37c3be19079e8ef9555
579 <simondumesny@gmail.com>
2021-09-02 19:07:04 +0200

    updated installation instruction

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #install a lot of things
 root=`pwd`
 pip install -r requirements.txt
@@ -6,8 +6,10 @@ cd front
 npm install
 export DISPLAY=:0.0 #for electron to work perfectly with ssh
 #install arduino required parts
-#even if there is a library to install manually
 arduino-cli config init
 arduino-cli core update-index
 arduino-cli core install arduino:avr
 arduino-cli lib install "RTClib"
+#manual installation of the dht11 library
+cd
+cp -r $root/Arduino/lib/dht11 /Arduino/libraries

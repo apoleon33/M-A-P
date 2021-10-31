d3074b40e1b5a31c8408d6c7f061d97f610a37b7
579 <simondumesny@gmail.com>
2021-10-31 19:04:09 +0100

    added a type-checking ton launch.sh and its recursive NSI gang gang

--- src/launch.sh
+++ src/launch.sh
@@ -6,25 +6,33 @@ two=$2
 random_var=4
 
 cd front
-#npm run build-type # run the "build-type" package script (useless if it does not work)
+# npm run build-type # run the "build-type" package script (useless if it does not work)
 
 cd
 echo "do you want to launch the simulator? [y/n]"
 read simu_bool
 
-if [ simu_bool == "y" ]
-then
-
+#recursive function to check the type of the actualisation variable
+wrong_parameter() {
 	echo "choose the actualisation time:"
 	read actualisation
+	if [[ $actualisation =~ ^[+-]?[0-9]+$ ]]; # if the actualisation time is an integer
+	then
+		# launch simulator and frontend
+		cd $root
+		python3 simulator.py $actualisation & cd front && npm start
+	else
+		echo "wrong type for the actualisation parameter, please retry"
+		wrong_parameter
+	fi
+}
 
-	#launch simulator and frontend
-	cd $root
-	python3 simulator.py $aactualisation & cd front && npm start
-
+if [ $simu_bool == "y" ]
+then
+	wrong_parameter
 else
 
-	#launch the serial communication and the frontend
+	# launch the serial communication and the frontend
 	cd $root
 	python3 serial_communication.py & cd front && npm start
 fi

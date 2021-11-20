0581955c7ee9d1544e97f7246b90a756412aa5e7
579 <simondumesny@gmail.com>
2021-11-20 18:37:24 +0100

    [install] improved installation instructions

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#install a lot of things
-#this script should be launched before televersing to the arduino
-root=`pwd`
+
+#clone the repo
+echo "cloning the M-A-P..."
+git clone https://github.com/apoleon33/M-A-P.git
+cd M-A-P
 
 #install required python packages
+echo "installing required python packages..."
 pip install -r requirements.txt
 
 #install required node packages
+echo("installing required npm packages")
 cd src/front
 npm install
-
-#manual installation of the dht11 library for the arduino
-#maybe useless
-cd
-echo "installing manually the dht11 arduino library"
-cp -r $root/src/Arduino/lib/dht11 $HOME/Arduino/libraries
-echo "done!"

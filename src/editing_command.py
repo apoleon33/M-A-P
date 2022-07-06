677e694ba18b17ae39dc5074813cb6f1dfe6138e
apoleon33 <simondumesny@gmail.com>
2022-07-06 16:08:08 +0200

    datetime wasnt imported for an obscure reason

--- src/editing_command.py
+++ src/editing_command.py
@@ -1,31 +1,36 @@
 import os
 import sqlite3
+import datetime
 import time
+
 
 class Database():
     '''everything linked with the database management'''
-    def __init__(self,db):
+
+    def __init__(self, db):
         self.location = db
         self.opening()
 
-    def insert_sql(self,temperature:int,humidity:int) -> None :
+    def insert_sql(self, temperature: int, humidity: int) -> None:
         '''insert data in the database'''
         time_now = datetime.date.fromtimestamp(time.time())
-        self.cursore.execute(f"INSERT INTO data VALUES( {str(time_now)} ,{temperature}, {humidity})" )
+        self.cursore.execute(
+            f"INSERT INTO data VALUES( {str(time_now)} ,{temperature}, {humidity})")
         self.db.commit()
 
-    def find_sql(self,date:str) -> list:
+    def find_sql(self, date: str) -> list:
         '''
         find data from a specific date
         not used at the moment
         The date have to be in the form "year-month-day"
         '''
-        self.cursore.execute(f"SELECT temperature,humidity FROM data WHERE date='{date}'")
+        self.cursore.execute(
+            f"SELECT temperature,humidity FROM data WHERE date='{date}'")
         old_data = self.cursore.fetchall()
         return old_data
 
     def opening(self):
-        self.db = sqlite3.connect(location)
+        self.db = sqlite3.connect(self.location)
         self.cursore = self.db.cursor()
 
     def closing(self):

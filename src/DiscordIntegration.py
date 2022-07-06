apoleon33/M-A-P

commit 071b13c495bf13bc8f0c41ecf72e1031c7a71b63

Revert "doc"
This reverts commit 8a105a099ffc4f1ca5a80ed452a934893231c2ec.

src/DiscordIntegration.py
@@ -1,3 +1,4 @@
+from rich import *
 from pypresence import Presence
 
 
@@ -12,17 +13,16 @@ class DiscordIntegration():
             self.RPC = Presence(self.rich_token)
             self.RPC.connect()
         except:
-            return False
+            pass
 
-    def update_presence(self, time: int, temperature: int = 0, humidity:int =0) -> None:
+    def update_presence(self, time: int, temperature: int = 0) -> None:
         try:
             self.RPC.update(large_image="plant",
                             large_text="M-A-P",
                             small_image="simulation",
                             small_text="using the simulator",
-                            details=f"temperature: {str(temperature)} °C",
-                            state=f"humidity: {str(humidity)} %",
+                            details="temperature: " + str(temperature) + " °C",
                             start=time,
                             buttons=[{"label": "github", "url": 'https://github.com/apoleon33/M-A-P'}])
         except:
-            return False
+            pass

src/algorithm.py
@@ -40,25 +40,42 @@ class Plante():
 		The algorithm that will manage the plant itself.
 		divided in 2 category,if we are in summer (spring + summer) or in winter (autumn + winter)
 		'''
+
 		time_now = datetime.date.fromtimestamp(time.time())
 		month = time_now.month  # if its winter/summer
 		if month >= 4 and month <= 9:  # summer
-			# i is the index wether its summer/winter
-			i = 1
-		else:  # winter
-			i = 0
-		
-		if self.temperature < self.ideal_temperature[i]:
-			port.write(b'A')
+			if self.temperature < self.ideal_temperature[1]:
+				port.write(b'A')
 
-		if self.taux1 < 20 and self.ideal_water[i] == "coupelle":
-			port.write(b'B')
 
-		if self.taux1 < 20 and self.ideal_water[i] == "sec":
-			self.sec_check += 1
-			if self.sec_check >= 2:
+			if self.taux1 < 20 and self.ideal_water[1] == "coupelle":
 				port.write(b'B')
-				self.sec_check = 0
 
-		if self.taux2 < 20 and self.ideal_water[i] == "pot":
-			port.write(b'C')
+
+			if self.taux1 < 20 and self.ideal_water[1] == "sec":
+				self.sec_check += 1
+				if self.sec_check >= 2:
+					port.write(b'B')
+					self.sec_check = 0
+
+			if self.taux2 < 20 and self.ideal_water[1] == "pot":
+				port.write(b'C')
+
+
+		else:  # winter
+			if self.temperature < self.ideal_temperature[0]:
+				port.write(b'A')
+
+
+			if self.taux1 < 20 and self.ideal_water[0] == "coupelle":
+				port.write(b'B')
+
+
+			if self.taux1 < 20 and self.ideal_water[0] == "sec":
+				self.sec_check += 1
+				if self.sec_check >= 2:
+					port.write(b'B')
+					self.sec_check = 0
+
+			if self.taux2 < 20 and self.ideal_temperature[0] == "pot":
+				port.write(b'C')

.gitmodules
@@ -1,6 +1,0 @@
-[submodule "plant-database"]
-	path = src/front/data/plant-database
-	url = https://github.com/M-A-P-Organisation/plant-database
-[submodule "bot"]
-	path = src/front/discord-bot
-	url = https://github.com/M-A-P-Organisation/discord-bot

install.sh
@@ -2,7 +2,7 @@
 
 #clone the repo
 echo "cloning the M-A-P..."
-git clone --recursive https://github.com/apoleon33/M-A-P.git
+git clone https://github.com/apoleon33/M-A-P.git
 cd M-A-P
 
 #install required python packages

src/editing_command.py
@@ -25,7 +25,7 @@ class Database():
         return old_data
 
     def opening(self):
-        self.db = sqlite3.connect(self.location)
+        self.db = sqlite3.connect(location)
         self.cursore = self.db.cursor()
 
     def closing(self):

src/front/.env
@@ -1,1 +1,0 @@
-TOKEN = 

src/front/.gitignore
@@ -9,5 +9,3 @@ data/temp_10.txt
 data/temp_20.txt
 data/temp_30.txt
 data/old.db
-bot.js
-.env

commit d0d64b041b84c466c9fa5bc37fbd630c09b99b4f

theorically fixed all errors that were going to happend 071b13c495bf13bc8f0c41ecf72e1031c7a71b63 broke a lot of things

src/DiscordIntegration.py
@@ -1,4 +1,3 @@
-from rich import *
 from pypresence import Presence
 
 
@@ -13,16 +12,17 @@ class DiscordIntegration():
             self.RPC = Presence(self.rich_token)
             self.RPC.connect()
         except:
-            pass
+            return False
 
-    def update_presence(self, time: int, temperature: int = 0) -> None:
+    def update_presence(self, time: int, temperature: int = 0, humidity: int = 0) -> None:
         try:
             self.RPC.update(large_image="plant",
                             large_text="M-A-P",
                             small_image="simulation",
                             small_text="using the simulator",
-                            details="temperature: " + str(temperature) + " °C",
+                            details=f"temperature: {str(temperature)} °C",
+                            state=f"humidity: {str(humidity)} %",
                             start=time,
                             buttons=[{"label": "github", "url": 'https://github.com/apoleon33/M-A-P'}])
         except:
-            pass
+            return False

src/algorithm.py
@@ -3,79 +3,63 @@ import time
 import datetime
 import serial
 
+
 class Plante():
-	def __init__(self,name):
-		with open("front/data/plant.json", "r") as main:
-			file = json.load(main)
+    def __init__(self, name):
+        with open("front/data/plant.json", "r") as main:
+            file = json.load(main)
 
-		self.name = name
-		self.ideal_temperature =  [
-			file[self.name]["temperature"]["hiver"],
-			file[self.name]["temperature"]["été"]
-		]
+        self.name = name
+        self.ideal_temperature = [
+            file[self.name]["temperature"]["hiver"],
+            file[self.name]["temperature"]["été"]
+        ]
 
-		self.ideal_humidity = [
-			file[self.name]["eau"]["hiver"],
-			file[self.name]["eau"]["été"]
-		]
+        self.ideal_humidity = [
+            file[self.name]["eau"]["hiver"],
+            file[self.name]["eau"]["été"]
+        ]
 
-		self.temperature = None
-		self.humidity = None
-		self.taux1 = None
-		self.taux2 = None
-		self.sec_check = 0
+        self.temperature = None
+        self.humidity = None
+        self.taux1 = None
+        self.taux2 = None
+        self.sec_check = 0
 
-	def setTaux(self,data1,data2):
-		self.taux1 = data1
-		self.taux2 = data2
+    def setTaux(self, data1, data2):
+        self.taux1 = data1
+        self.taux2 = data2
 
-	def setTemperature(self,temperature):
-		self.temperature = temperature
+    def setTemperature(self, temperature):
+        self.temperature = temperature
 
-	def setHumidity(self,humidity):
-		self.humidity = humidity
+    def setHumidity(self, humidity):
+        self.humidity = humidity
 
-	def decision(self,port:object):
-		'''
-		The algorithm that will manage the plant itself.
-		divided in 2 category,if we are in summer (spring + summer) or in winter (autumn + winter)
-		'''
+    def decision(self, port: object):
+        '''
+        The algorithm that will manage the plant itself.
+        divided in 2 category,if we are in summer (spring + summer) or in winter (autumn + winter)
+        '''
+        time_now = datetime.date.fromtimestamp(time.time())
+        month = time_now.month  # if its winter/summer
+        if month >= 4 and month <= 9:  # summer
+            # i is the index wether its summer/winter
+            i = 1
+        else:  # winter
+            i = 0
 
-		time_now = datetime.date.fromtimestamp(time.time())
-		month = time_now.month  # if its winter/summer
-		if month >= 4 and month <= 9:  # summer
-			if self.temperature < self.ideal_temperature[1]:
-				port.write(b'A')
+        if self.temperature < self.ideal_temperature[i]:
+            port.write(b'A')
 
+        if self.taux1 < 20 and self.ideal_water[i] == "coupelle":
+            port.write(b'B')
 
-			if self.taux1 < 20 and self.ideal_water[1] == "coupelle":
-				port.write(b'B')
+        if self.taux1 < 20 and self.ideal_water[i] == "sec":
+            self.sec_check += 1
+            if self.sec_check >= 2:
+                port.write(b'B')
+                self.sec_check = 0
 
-
-			if self.taux1 < 20 and self.ideal_water[1] == "sec":
-				self.sec_check += 1
-				if self.sec_check >= 2:
-					port.write(b'B')
-					self.sec_check = 0
-
-			if self.taux2 < 20 and self.ideal_water[1] == "pot":
-				port.write(b'C')
-
-
-		else:  # winter
-			if self.temperature < self.ideal_temperature[0]:
-				port.write(b'A')
-
-
-			if self.taux1 < 20 and self.ideal_water[0] == "coupelle":
-				port.write(b'B')
-
-
-			if self.taux1 < 20 and self.ideal_water[0] == "sec":
-				self.sec_check += 1
-				if self.sec_check >= 2:
-					port.write(b'B')
-					self.sec_check = 0
-
-			if self.taux2 < 20 and self.ideal_temperature[0] == "pot":
-				port.write(b'C')
+        if self.taux2 < 20 and self.ideal_water[i] == "pot":
+            port.write(b'C')